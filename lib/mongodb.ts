b5e15b4c3cc6d73ce1a4265de7e82fc8c54931e8
Ashwinpatel7 <104048946+Ashwinpatel7@users.noreply.github.com>
2025-06-24 15:56:50 +0530

    Done making the app

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,10 +6,19 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable')
 }
 
-let cached = global.mongoose
+interface MongooseCache {
+  conn: typeof mongoose | null
+  promise: Promise<typeof mongoose> | null
+}
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+declare global {
+  var mongoose: MongooseCache | undefined
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null }
+
+if (!global.mongoose) {
+  global.mongoose = cached
 }
 
 async function dbConnect() {
@@ -18,13 +27,7 @@ async function dbConnect() {
   }
 
   if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    }
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose
-    })
+    cached.promise = mongoose.connect(MONGODB_URI)
   }
 
   try {
